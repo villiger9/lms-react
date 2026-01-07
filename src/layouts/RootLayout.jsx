ef965944f74f6b9352bf3e163cb2f211852a648f
villiger9 <ali.shaker93@gmail.com>
2026-01-07 23:28:08 +0300

    updated router logic and created RootLayout

---
+++ src/layouts/RootLayout.jsx
@@ -0,0 +1,18 @@
+import { NavLink, Outlet } from "react-router-dom";
+
+export default function RootLayout() {
+  return (
+    <div>
+      <header>
+        <nav>
+          <h1>CourseRouter</h1>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="about">About</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
