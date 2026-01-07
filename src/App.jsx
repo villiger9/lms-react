a0cb0943ae9b9f86fb32443efe33beaa9cde6275
villiger9 <ali.shaker93@gmail.com>
2026-01-07 22:17:13 +0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
+import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+
+//pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+
 function App() {
   return (
-    <>
-      <div>lms</div>
-    </>
+    <BrowserRouter>
+      <header>
+        <nav>
+          <h1>CourseRouter</h1>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="about">About</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
