9a38fb37b13b53801756962a48b0d31042000118
villiger9 <ali.shaker93@gmail.com>
2026-01-08 22:50:38 +0300

    form action in contact

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import {
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Faq from "./pages/help/Faq";
-import Contact from "./pages/help/Contact";
+import Contact, { contactAction } from "./pages/help/Contact";
 import NotFound from "./NotFound";
 import Courses, { coursesLoader } from "./pages/courses/Courses";
 import CourseDetails, {
@@ -29,7 +29,7 @@ const router = createBrowserRouter(
       <Route path="about" element={<About />} />
       <Route path="help" element={<HelpLayout />}>
         <Route path="faq" element={<Faq />} />
-        <Route path="contact" element={<Contact />} />
+        <Route path="contact" element={<Contact />} action={contactAction} />
       </Route>
 
       <Route
--- src/pages/help/Contact.jsx
+++ src/pages/help/Contact.jsx
@@ -1,8 +1,11 @@
+import { Form, redirect, useActionData } from "react-router-dom";
+
 export default function Contact() {
+  const data = useActionData();
   return (
     <div className="contact">
       <h3>Contact Us</h3>
-      <form>
+      <Form method="post" action="/help/contact">
         <label>
           <span>Your email:</span>
           <input type="email" name="email" required />
@@ -12,7 +15,27 @@ export default function Contact() {
           <textarea name="message" required></textarea>
         </label>
         <button>Submit</button>
-      </form>
+        {data && data.error && <p>{data.error}</p>}
+      </Form>
     </div>
   );
 }
+
+export const contactAction = async ({ request }) => {
+  console.log(request);
+  const data = await request.formData();
+  const submission = {
+    email: data.get("email"),
+    message: data.get("message"),
+  };
+
+  console.log(submission);
+
+  //send post request
+  if (submission.message.length < 10) {
+    return { error: "message must be longer" };
+  }
+
+  //redirect the user
+  return redirect("/");
+};
